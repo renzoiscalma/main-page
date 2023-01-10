e30b5c02f0e93085e351fecb3e491f6569667689
Renzo Calma <jlpcalma@gmail.com>
2023-01-09 21:56:54 -0400

    Added body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import {image1, image2} from "../utils/image";
+const headline = {};
+const gridData = [{}];
 
 const Body = () => {
 	return (
 		<>
-			Body!
+			<Grid container spacing={2}>
+				{/* <Headline></Headline> */}
+				{/* { Array.map(() => (<Grid> </Grid>))} */}
+				<Grid item xs={6}>
+						<Typography>
+							Easier for you
+						</Typography>
+						<Typography>
+							Spend less than 5 seconds sending your next invoice
+						</Typography>
+						<Typography>
+							Spend more time doing the work you want to be doing. 
+							TwoCards meets the needs of freelancers, 
+							making invoicing swift, simple and secure.
+						</Typography>
+				</Grid>
+				<Grid item xs={6}>
+						<img
+							src={image1}
+							height={480}
+						/>
+				</Grid>
+				<Grid item xs={6}>
+						<img
+							src={image2+"?w=480&h=480"}
+							height={480}
+						/>
+				</Grid>
+				<Grid item xs={6}>
+						<Typography>
+							Easier for you
+						</Typography>
+						<Typography>
+							Spend less than 5 seconds sending your next invoice
+						</Typography>
+						<Typography>
+							Spend more time doing the work you want to be doing. 
+							TwoCards meets the needs of freelancers, 
+							making invoicing swift, simple and secure.
+						</Typography>
+				</Grid>
+			</Grid>
 		</>
 	)
 };
---
+++ src/utils/image.jsx
@@ -0,0 +1,7 @@
+import image1 from '../media/image1.svg';
+import image2 from '../media/image2.png';
+
+export {
+	image1,
+	image2
+};
